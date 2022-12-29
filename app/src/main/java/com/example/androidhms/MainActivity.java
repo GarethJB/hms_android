35c661266869c893647401c7f49e6a49c304a8f4
JB <108816562+GarethJB@users.noreply.github.com>
2022-12-29 16:33:44 +0900

    update

--- app/src/main/java/com/example/androidhms/MainActivity.java
+++ app/src/main/java/com/example/androidhms/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.androidhms;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.conn.ApiClient;
+import com.example.conn.CommonMethod;
 
 //텍스트 사이즈
 //네비게이션 20dp
@@ -19,13 +21,22 @@ import com.example.conn.ApiClient;
 //text_color
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "로그";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ApiClient.setBASEURL("http://192.168.0.116/??/");
+        ApiClient.setBASEURL("http://192.168.0.116/middle/");
+
+        new CommonMethod().setParams("a", "1").setParams("b", "2").sendPost("hmstest", new CommonMethod.CallBackResult() {
+            @Override
+            public void result(boolean isResult, String data) {
+                Log.d(TAG, "result: " + isResult);
+                Log.d(TAG, "result: " + data);
+            }
+        });
 
 
 
