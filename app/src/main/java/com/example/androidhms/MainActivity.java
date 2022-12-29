581dac6ceebf4314fff60851cf8760a3a3e7d50a
JB <108816562+GarethJB@users.noreply.github.com>
2022-12-29 16:04:25 +0900

    conn create

--- app/src/main/java/com/example/androidhms/MainActivity.java
+++ app/src/main/java/com/example/androidhms/MainActivity.java
@@ -1,8 +1,22 @@
 package com.example.androidhms;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.conn.ApiClient;
+
+//텍스트 사이즈
+//네비게이션 20dp
+//타이틀 24dp
+//하위항목 20dp
+//내용 16dp
+
+//컬러
+//main_color
+//second_color
+//white
+//text_color
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ApiClient.setBASEURL("http://192.168.0.116/??/");
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/androidhms/login/CustomerLoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidhms.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidhms.R;
+
+public class CustomerLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login);
+
+
+    }
+}
