26c3b3f6127dc30e7b3a75434f1831610cd7550a
JB <108816562+GarethJB@users.noreply.github.com>
2023-01-03 08:16:48 +0900

    update fragmetn

--- app/src/main/java/com/example/androidhms/customer/info/InfoFragment.java
+++ app/src/main/java/com/example/androidhms/customer/info/InfoFragment.java
@@ -6,17 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidhms.R;
 
 
 public class InfoFragment extends Fragment {
-
+    RecyclerView rcv_reservation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_info, container, false);
+
+        rcv_reservation = v.findViewById(R.id.rcv_reservation);
+
+        ReservationAdapter adapter = new ReservationAdapter(inflater, getContext());
+
+        rcv_reservation.setAdapter(adapter);
+        rcv_reservation.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
 
         return v;
     }
