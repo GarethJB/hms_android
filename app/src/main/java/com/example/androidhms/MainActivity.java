77cc72486f3ccbe8bb0167ca12f9ec69bcba36ad
JB <108816562+GarethJB@users.noreply.github.com>
2023-01-04 15:03:55 +0900

    main activity update

--- app/src/main/java/com/example/androidhms/MainActivity.java
+++ app/src/main/java/com/example/androidhms/MainActivity.java
@@ -1,20 +1,16 @@
 package com.example.androidhms;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidhms.databinding.ActivityMainBinding;
-import com.example.androidhms.receptionoffice.reception.ReceptionActivity;
 import com.example.conn.ApiClient;
 import com.example.conn.RetrofitMethod;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     String TAG = "로그";
     ActivityMainBinding bind;
 
@@ -25,9 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(bind.getRoot());
 
 
-    logo= findViewById(R.id.logo);
 
-    logo.setOnClickListener(this);
 
         ApiClient.setBASEURL("http://192.168.0.116/middle/");
 
@@ -41,12 +35,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.logo){
-            Intent intent = new Intent(MainActivity.this, ReceptionActivity.class);
-            startActivity(intent);
-        }
-    }
+
 }
 
