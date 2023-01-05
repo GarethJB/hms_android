a1a2928b765fba03ef07e0d6f3bc7b9310b17c4c
JB <108816562+GarethJB@users.noreply.github.com>
2023-01-05 09:41:52 +0900

    customer login update

---
+++ app/src/main/java/com/example/androidhms/customer/CustomerLoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidhms.customer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidhms.R;
+
+public class CustomerLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login);
+    }
+}
