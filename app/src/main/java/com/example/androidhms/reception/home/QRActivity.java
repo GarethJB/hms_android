c4151d1f1f602aea4320f2ffb49968369bdd2832
hyewoon <likethebegin@naver.com>
2023-01-09 00:09:25 +0900

    commit

---
+++ app/src/main/java/com/example/androidhms/reception/home/QRActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidhms.reception.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidhms.R;
+
+public class QRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qractivity);
+
+
+    }
+}
