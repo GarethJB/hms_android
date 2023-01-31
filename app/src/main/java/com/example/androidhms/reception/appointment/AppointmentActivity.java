1618d3865764c610f12a1c24f2a31d1068f2a58e
hyewoon <likethebegin@naver.com>
2023-01-31 13:57:42 +0900

    commit

--- app/src/main/java/com/example/androidhms/reception/appointment/AppointmentActivity.java
+++ app/src/main/java/com/example/androidhms/reception/appointment/AppointmentActivity.java
@@ -76,30 +76,31 @@ public class AppointmentActivity extends AppCompatActivity {
 
 
 
-     /*   ArrayAdapter department_list = ArrayAdapter.createFromResource(this, R.array.department_list, android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter doctor_list = ArrayAdapter.createFromResource(this, R.array.doctor_list, android.R.layout.simple_spinner_dropdown_item);
         //내가 지정한 리스트department_list, 기본 제공하는 드롬다운 simple_spinner_dropdown_item
-        department_list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        bind.spinnerDoctor.setAdapter(department_list);
+        doctor_list.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bind.spinnerDoctor.setAdapter(doctor_list);
 
         bind.spinnerDoctor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 doctor_id = position;
 
-               // doctor_name =  bind.spinnerDoctor.getSelectedItem().toString();
-               // Log.d("로그", "onItemSelected: " +doctor_name );
+                doctor_name =  bind.spinnerDoctor.getSelectedItem().toString();
+               Log.d("로그", "onItemSelected: " +doctor_name );
+                getAppointment();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
             }
-        });*/
+        });
 
 
     }
 
     public void getAppointment(){
-        new RetrofitMethod().setParams("time",date2 ).sendPost("apointmentList.re", (isResult, data) -> {
+        new RetrofitMethod().setParams("time",date2 ).setParams("doctor_name",doctor_name).sendPost("apointmentList.re", (isResult, data) -> {
             Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd hh:mm:ss").create();
             list = gson.fromJson(data, new TypeToken<ArrayList<MedicalReceiptVO>>() {}.getType());
             Log.d("로그", "getAppointment: "+ "이름값" + data);
